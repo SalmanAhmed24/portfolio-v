c2d4c6a3358f48d6713b0ddb1619d69cb41ecad4
unknown <salman.ahmed.abbasi.24@gmail.com>
2021-12-08 16:56:56 +0500

    made changes in CSS file and added favicon

--- components/portfolio.js
+++ components/portfolio.js
@@ -16,32 +16,20 @@ function Portfolio() {
 			<h1 className="heading">Portfolio</h1>
 			<div className="portfolioInfo">
 				<div className="projectWrap" style={{ width: '600px' }}>
-					<div
-						className="projectImgWrap"
-						style={{ backgroundImage: `url(/images/bookatutor.png)`, height: '300px' }}
-					>
+					<div className="projectImgWrap" style={{ backgroundImage: `url(/images/bookatutor.png)` }}>
 						<div className="blurDiv" onClick={() => viewProject('https://bookatutor.net/')} />
 					</div>
-					<div
-						className="projectImgWrap2"
-						style={{ backgroundImage: `url(/images/content.png)`, height: '300px' }}
-					>
+					<div className="projectImgWrap2" style={{ backgroundImage: `url(/images/content.png)` }}>
 						<div className="blurDiv" onClick={() => viewProject('https://contentproz.com/')} />
 					</div>
 				</div>
-				<div className="projectWrap" style={{ width: '400px' }}>
-					<div
-						className="projectImgWrap"
-						style={{ backgroundImage: `url(/images/meetshm.png)`, height: '600px' }}
-					>
+				<div className="projectWrap2">
+					<div className="projectImgWrap3" style={{ backgroundImage: `url(/images/meetshm.png)` }}>
 						<div className="blurDiv" onClick={() => viewProject('http://meetshm.com/')} />
 					</div>
 				</div>
-				<div className="projectWrap" style={{ width: '400px' }}>
-					<div
-						className="projectImgWrap"
-						style={{ backgroundImage: `url(/images/risetech.png)`, height: '600px' }}
-					>
+				<div className="projectWrap2">
+					<div className="projectImgWrap4" style={{ backgroundImage: `url(/images/risetech.png)` }}>
 						<div className="blurDiv" onClick={() => viewProject('https://risetech.pk/')} />
 					</div>
 				</div>
